950c90bfd540e4dbb5329a4a6544b1f269c0802e
Eric Maina Gathoni <ericbro29@gmail.com>
2020-10-16 18:11:35 +0300

    Imports: Add component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Logo } from './Logo';
 export { default as TopNavigation } from './TopNavigation';
 export { default as SideNavigation } from './SideNavigation';
+export { default as TaskActions } from './TaskActions';
+export { default as ListActions } from './ListActions';
+export { default as AddNewForm } from './AddNewForm';
+export { default as Task } from './Task';

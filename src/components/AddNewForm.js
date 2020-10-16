1b784cbd9311f1e98d7d90487a2894b18e3a491c
Eric Maina Gathoni <ericbro29@gmail.com>
2020-10-16 18:16:36 +0300

    Add form for manipulating the data.

---
+++ src/components/AddNewForm.js
@@ -0,0 +1,81 @@
+import React, { useContext, useState } from 'react';
+
+import { ListContext } from '../store';
+
+import styles from '../styles/form.module.css';
+
+const AddForm = () => {
+  const {
+    form,
+    SHOW_FORM,
+    ADD_ITEM,
+    RENAME_ITEM,
+    GET_LISTS,
+    CURRENTLY_ACTIVE,
+  } = useContext(ListContext);
+  const [title, setTitle] = useState('');
+
+  const handleClose = () => {
+    setTitle('');
+    SHOW_FORM(null, false);
+  };
+  const handleTitle = (e) => setTitle(e.target.value);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      title.length !== 0 &&
+      form.type !== 'rename list' &&
+      form.type !== 'rename task'
+    ) {
+      await ADD_ITEM(form.type, title);
+      if (form.type === 'list') {
+        const data = GET_LISTS();
+        CURRENTLY_ACTIVE('list', data[0].id);
+      }
+      handleClose();
+    }
+
+    if (
+      title.length !== 0 &&
+      (form.type === 'rename list' || form.type === 'rename task')
+    ) {
+      if (form.type === 'rename list') {
+        RENAME_ITEM({ type: 'list', title });
+        handleClose();
+      }
+    }
+  };
+
+  return (
+    <div className={styles.add_form__container}>
+      <form className={styles.add_form} onSubmit={handleSubmit}>
+        <div className='form_group'>
+          <label htmlFor='title'>Enter the title</label>
+          <input
+            type='text'
+            id='title'
+            className='form_control'
+            placeholder='New title'
+            required
+            autoComplete='off'
+            autoFocus='on'
+            value={title}
+            onChange={handleTitle}
+          />
+        </div>
+        <div className='form_group'>
+          <button className='form_submit'>Save</button>
+          <button
+            type='button'
+            className={styles.form_cancel}
+            onClick={handleClose}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddForm;
--- src/layouts/TaskLayout.js
+++ src/layouts/TaskLayout.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { SideNavigation } from '../components';
+import { SideNavigation, AddNewForm } from '../components';
 
 import layoutStyles from '../styles/layouts.module.css';
+import { ListContext } from '../store';
 
 const TaskLayout = ({ children, page }) => {
+  const { form } = useContext(ListContext);
+
   return (
-    <div class={layoutStyles.task_layout__container}>
+    <div className={layoutStyles.task_layout__container}>
       <div className={layoutStyles.task_layout__navigation}>
         <SideNavigation action={page} />
       </div>
-      <div class={layoutStyles.task_layout__child}>{children}</div>
+      <div className={layoutStyles.task_layout__child}>
+        {form.visible ? <AddNewForm /> : null}
+        {children}
+      </div>
     </div>
   );
 };

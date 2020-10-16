ac2e9c46e4e490fffefc7977b2ce2696d27a9c63
Eric Maina Gathoni <ericbro29@gmail.com>
2020-10-16 18:14:24 +0300

    Fix jsx class name typo.

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -6,9 +6,9 @@ import layoutStyles from '../styles/layouts.module.css';
 
 const DefaultLayout = ({ children }) => {
   return (
-    <div class={layoutStyles.default_layout__container}>
+    <div className={layoutStyles.default_layout__container}>
       <TopNavigation />
-      <div class={layoutStyles.default_layout__child}>{children}</div>
+      <div className={layoutStyles.default_layout__child}>{children}</div>
     </div>
   );
 };

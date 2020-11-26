dc0bc17effba6d0606642cb501167eca59a5e988
Eric Maina Gathoni <ericbro29@gmail.com>
2020-11-26 20:01:26 +0300

    Add about page export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as HOME_PAGE } from './home-page';
 export { default as LIST_PAGE } from './list-page';
 export { default as TASK_PAGE } from './task-page';
+export { default as ABOUT_PAGE } from './about-page';

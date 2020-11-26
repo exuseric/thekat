63de4cc10ba657565e80045f0c133348097558c4
Eric Maina Gathoni <ericbro29@gmail.com>
2020-11-26 20:00:59 +0300

    Add routing to the about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { HOME_PAGE, LIST_PAGE, TASK_PAGE } from './pages';
+import { HOME_PAGE, LIST_PAGE, TASK_PAGE, ABOUT_PAGE } from './pages';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
           <Route exact path='/' component={HOME_PAGE} />
           <Route exact path='/list/' component={LIST_PAGE} />
           <Route exact path='/list/:list/:task' component={TASK_PAGE} />
+          <Route exact path='/about' component={ABOUT_PAGE} />
         </Switch>
       </Router>
     </main>
